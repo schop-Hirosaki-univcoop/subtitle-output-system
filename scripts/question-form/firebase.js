890ee398a3ac9ec10e1aacb0ed9cd01ce4bd33ab
MarKvEditor <markveditor181935@gmail.com>
2025-10-10 21:20:27 +0900

    Refactor question intake form into modular structure

---
+++ scripts/question-form/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+export function getDatabaseInstance(config) {
+  const apps = getApps();
+  const app = apps.length ? getApp() : initializeApp(config);
+  return getDatabase(app);
+}
