c9b69a51a67482b397f86e0da6a04ab41314254e
MarKvEditor <markveditor181935@gmail.com>
2025-10-11 22:25:31 +0900

    Route question form submissions through RTDB

--- scripts/question-form/constants.js
+++ scripts/question-form/constants.js
@@ -1,11 +1,8 @@
-export const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxYtklsVbr2OmtaMISPMw0x2u0shjiUdwkym2oTZW7Xk14pcWxXG1lTcVC2GZAzjoba_pQ/exec";
-
 export const GENRE_OPTIONS = ["学び", "活動", "暮らし", "食・スポット", "移動・季節", "その他"];
 export const TOKEN_PARAM_KEYS = ["token", "t", "key"];
 export const MAX_RADIO_NAME_LENGTH = 20;
 export const MAX_QUESTION_LENGTH = 60;
 export const FORM_VERSION = "question-form@2024.11";
-export const QUESTION_SUBMISSIONS_PATH = "questionIntake/submissions";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBh54ZKsM6uNph61QrP-Ypu7bzU_PHbNcY",
